97e9b0dc852be94824eba2bfb83029215ee1e414
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2022-04-17 23:21:35 +0000

    commit en retard

---
+++ src/ClientWS.java
@@ -0,0 +1,24 @@
+import proxy.BanqueService;
+import proxy.BanqueWS;
+import proxy.Compte;
+
+import java.util.List;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        BanqueService stubWS = new BanqueWS().getBanqueServicePort();
+        System.out.println(stubWS.conversionEuroToDH(443));
+        Compte cp = stubWS.getCompte(2L) ;
+        System.out.println(cp.getCode());
+        System.out.println(cp.getSolde());
+        List<Compte> cptes = stubWS.listComptes();
+        cptes.forEach(c -> {
+            System.out.println("______________________");
+            System.out.println(c.getCode());
+            System.out.println(c.getSolde());
+        });
+
+
+
+    }
+}
